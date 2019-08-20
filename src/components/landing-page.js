5d655fe1cf593050740ff9f6b212255ee25a07f7
Rebecca Bob <rebecca.rupel@gmail.com>
2019-08-20 15:13:13 -0700

    wireframe landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './landing-page.css';
-import Navbar from './navbar'; 
-import SideDrawer from './sidedrawer'; 
-import Backdrop from './backdrop'; 
-
+import Navbar from './navbar';
+import {Link} from 'react-router-dom'; 
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div>
-         <p>Content!</p>
+      <div className="home">
+        <main>
+          <div className="landingpage-container">
+              <div className="landingpage-details-container">
+                <h1 className="welcome-header">Welcome</h1>
+                <div className="links">
+                  <div className="art-link"><Link to="/">Fine Art</Link></div>
+                  <div className="web-link"><Link to="/">Web Design/Development</Link></div>
+                </div>
+              </div>
+          </div>
+          {/* <div className="space"></div> */}
+          {/* <div className="containers">
+              <div className="art-container">
+                <h2>Fine Art</h2>
+              </div>
+              <div className="web-container">
+                <h2>Web Design/Development</h2>
+              </div>
+          </div> */}
+          {/* <div className="space"></div> */}
+        </main>
       </div>
     );
   }
