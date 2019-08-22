3482fa306c77d581be49926b4290ced1811c0e22
Rebecca Bob <rebecca.rupel@gmail.com>
2019-08-22 15:07:27 -0700

    about layout

--- src/components/about.js
+++ src/components/about.js
@@ -5,8 +5,10 @@ import './about.css';
 class About extends React.Component {
     render() {
         return (
-            <div>
-            
+            <div className="container3">
+                <h1 className="about-title">About</h1>
+                <p className="about-paragraph">This is info about me!</p>
+                <div className="about-image">Place holder for image</div>
             </div>
           );
     }
